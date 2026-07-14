582a695951625bd497df4f910152e48a4d050365
veeranjay singh <veeranjaysingh2601@gmail.com>
2026-07-15 01:42:43 +0530

    scan mode fixes

--- app/api/scan/route.ts
+++ app/api/scan/route.ts
@@ -51,7 +51,9 @@ export async function POST(req: NextRequest) {
 
         const modelsToTry = [
             "gemini-2.5-flash",
-            "gemini-2.5-flash-lite",
+            "gemini-3.1-flash-lite",
+            "gemini-3-flash-preview",
+            "gemini-3.5-flash"
         ];
 
         let text = null;
--- app/api/solve/route.ts
+++ app/api/solve/route.ts
@@ -40,8 +40,10 @@ export async function POST(req: NextRequest) {
         `;
 
         const modelsToTry = [
-            "gemini-2.5-flash",
-            "gemini-2.5-flash-lite"
+            "gemini-3.5-flash",
+            "gemini-3-flash-preview",
+            "gemini-3.1-flash-lite",
+            "gemini-2.5-flash"
         ];
 
         let rawText = null;
